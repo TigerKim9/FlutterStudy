d8c4222ba4b7c174f85ed6af795c8ed8af78eede
TigerKim9 <kkm9499@gmail.com>
2022-08-23 02:08:56 +0900

    인프런 플러터 강의

---
+++ android/app/src/main/kotlin/ai/kkm/first_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package ai.kkm.first_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,  //오른쪽 위 debug 배너 없애기
+      home: HomeScreen()));
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //핫 리로드는 build 함수가 있는 것만 다시 렌더한다.
+    return Scaffold(
+      backgroundColor: Color(0xFFF99231),//hex Code로 정확한 색상을 넣어보자
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,  //Column의 default는 맨 위 정렬이므로 센터 정렬 해줌.
+        children: [
+          Image.asset(
+              "asset/img/logo.png"
+          ),
+          CircularProgressIndicator(
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
