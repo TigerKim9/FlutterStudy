caaca92b4c8750ca13037856821a842fc10a9d58
TigerKim9 <kkm9499@gmail.com>
2022-08-28 07:39:44 +0900

    statefulwidget 개념 및 작성 법..

--- statefulwidget/lib/screen/home_screen.dart
+++ statefulwidget/lib/screen/home_screen.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget{
   final Color color;
 
   const HomeScreen({
+    required this.color,
+    Key? key
+  }): super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: widget.color,
+    );
+  }
+
+
+}
+
+class _HomeScreen extends StatelessWidget {
+  final Color color;
+
+  const _HomeScreen({
     required this.color,
     Key? key,
   }) : super(key: key);
