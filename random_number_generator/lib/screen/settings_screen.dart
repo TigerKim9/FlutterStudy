69a2f6f66b84b0cf62d29415c2be677daaa7e6f4
TigerKim9 <kkm9499@gmail.com>
2022-09-01 15:53:18 +0900

    Slider 생성 및 수치 변화 그려주기

--- random_number_generator/lib/screen/settings_screen.dart
+++ random_number_generator/lib/screen/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -8,12 +9,43 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 10000;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Setting"
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Row(
+                children: maxNumber.toInt()
+                    .toString()
+                    .split('')
+                    .map((e) => Image.asset(
+                          'asset/img/$e.png',
+                          width: 50,
+                          height: 70,
+                        ))
+                    .toList(),
+              ),
+            ),
+            Slider(
+                value: maxNumber,
+                min: 10000,
+                max: 1000000,
+                onChanged: (double val) {
+                  setState((){//값이 변하지만 슬라이드의 수치를 그리지 않기 때문에 setState로 계속 슬라이드를 그려내야한다.
+                  maxNumber = val;
+
+                  });
+                }),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(primary: RED_COLOR),
+                child: Text("저장"))
+          ],
         ),
       ),
     );
