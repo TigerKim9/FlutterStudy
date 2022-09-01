9b8434780ca60b03d4b7fd0ad65e462535d1f1c5
TigerKim9 <kkm9499@gmail.com>
2022-09-01 15:41:32 +0900

    Navigator 다른 화면으로 넘기기

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/screen/settings_screen.dart';
 
 import '../constant/color.dart';
 
@@ -10,6 +13,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<int> RandomNumber =[
+    123,
+    456,
+    789,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +41,14 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
                     IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context){
+                              return SettingsScreen();
+                            }
+                            )
+                          );
+                        },
                         icon: Icon(
                           Icons.settings,
                           color: RED_COLOR,
@@ -42,11 +58,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 Expanded(
                     child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          123,
-                          456,
-                          789,
-                        ].asMap().entries
+                        children:
+                            RandomNumber.asMap().entries
                             .map((x) => Padding(
                               padding: EdgeInsets.only(bottom: x.key==2?0: 16.0),// 맨 밑 숫자는 패딩 넣지 않기
                               child: Row(
@@ -62,13 +75,27 @@ class _HomeScreenState extends State<HomeScreen> {
                         ).toList()
                     )
                 ),
-                SizedBox(
-                    width: double.infinity,
+                SizedBox(   //Container를 써도 되지만 SizedBox가 아주 조금 빠른 것과 모두 Container를 쓰면 코드를 구분할 때 어려우므로 개발자들의 약속으로 요소에는 SizedBox를 써주는 고러한 느낌.
+                    width: double.infinity,   //화면 끝까지 버튼 길이 늘이기
                     child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           primary: RED_COLOR, //활성화 되었을 시 색
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          final rand = Random();
+
+                          final Set<int> newNumbers={};
+
+                          while(newNumbers.length !=3){         //중복된 숫자를 피하고 싶을 때 Set 자료형과 while 문을 사용해주면 간단하게 처리 가능
+                            final number = rand.nextInt(1000);
+
+                            newNumbers.add(number);
+
+                            setState(() {
+                              RandomNumber = newNumbers.toList();
+                            });
+                          }
+                        },
                         child: Text('생성하기')))
               ],
             ),
