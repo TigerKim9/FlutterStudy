948a3bf424d5341d2de5664939dd26c4f916f017
TigerKim9 <kkm9499@gmail.com>
2023-06-15 12:43:47 +0900

    Flutter Study - login_dice

---
+++ codingchef/lib/statefulStudy.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StateStudy extends StatefulWidget {
+  const StateStudy({Key? key}) : super(key: key);
+
+  @override
+  State<StateStudy> createState() => _StateStudyState();
+}
+
+class _StateStudyState extends State<StateStudy> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
